01074509ff00ec6009aacb9034969c4bee2eb98a
Marko Šterk <marko_sterk@hotmail.com>
2026-05-18 15:55:13 +0200

    Message formatter bug fix

--- packages/patera-aiinterface/src/patera/aiinterface/service_provider/openai.py
+++ packages/patera-aiinterface/src/patera/aiinterface/service_provider/openai.py
@@ -299,8 +299,7 @@ class OpenAIServiceProvider(BaseServiceProvider):
             )  # type: ignore
 
     def format_message(self, user_prompt: str, **kwargs) -> str:
-        msg: str = ""
         if kwargs:
             for key, value in kwargs.items():
-                msg = user_prompt.replace(f"<:{key}>", value)
-        return msg
+                user_prompt = user_prompt.replace(f"<:{key}>", value)
+        return user_prompt
