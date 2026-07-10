5417d0dfcf3702a800de1abf388eb195e23c09a5
Marko Šterk <marko_sterk@hotmail.com>
2026-07-10 10:33:21 +0200

    Check if static page exists. If not, raise 404 - not found

--- src/patera/static_pages.py
+++ src/patera/static_pages.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING, Any  # noqa: F401
-import os
-
+from pathlib import Path
 from .media_types import MediaType
 from .controller import Controller, get, produces, after_init
 from .request import Request
 from .response import Response
+from .exceptions import StaticPageNotFound
 
 if TYPE_CHECKING:
     from .patera import Patera  # noqa: F401
@@ -17,10 +17,8 @@ class StaticPages(Controller["Patera[Any]"]):
         """
         Add template path to Jinja2 templates search path.
         """
-        path: str = os.path.join(
-            self.app.root_path, self.app.configs.STATIC_PAGES_DIR.lstrip("/\\")
-        )
-        self.app.add_template_path(path)
+
+        self.app.add_template_path(str(self.static_pages_path))
 
     @get("/<path:page>")
     @produces(MediaType.TEXT_HTML)
@@ -32,4 +30,16 @@ class StaticPages(Controller["Patera[Any]"]):
         if not page.endswith(".html"):
             page += ".html"
 
+        path = self.static_pages_path / page
+        if not path.exists():
+            raise StaticPageNotFound(page)
+
         return await req.res.html(page)
+
+    @property
+    def static_pages_path(self) -> Path:
+        path: Path = Path(
+            self.app.root_path
+        ) / self.app.configs.STATIC_PAGES_DIR.lstrip("/\\")
+        path.resolve()
+        return path
